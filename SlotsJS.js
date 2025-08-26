9f9c90f8a2c811c0877356c32e02e00a69e6b2d1
p6exe <richardnot437@gmail.com>
2025-08-26 19:30:37 -0400

    added function for calculating the total winnings

--- SlotsJS.js
+++ SlotsJS.js
@@ -102,7 +102,7 @@ const transpose = (reels) => {
     return rows
 }
 
-const printROws = (rows) => {
+const printRows = (rows) => {
     for (const row of rows) {
         let rowString = "";
         for (const [i,symbol] of row.entries()) {
@@ -115,9 +115,34 @@ const printROws = (rows) => {
     }
 }
 
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+
+    for (let row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols){
+            if(symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+
+    return winnings;
+}
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 const reels = spin();
 const rows = transpose(reels);
-print(rows);
+printRows(rows);
+const winnings = getWinnings(rows, bet, numberOfLines);
+console.log("You won, $" + winnings.toString())
