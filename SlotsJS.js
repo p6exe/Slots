7f9cca2811f82667d5afce47b216d23b964649bf
p6exe <richardnot437@gmail.com>
2025-08-26 16:03:59 -0400

    added slot characters and their values

--- SlotsJS.js
+++ SlotsJS.js
@@ -11,6 +11,23 @@ function deposit(){
 
 const prompt = require("prompt-sync")(); // uses the package for user input
 
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A":2,
+    "B":4,
+    "C":6,
+    "D":8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
 const deposit = () => {
     while(true) {
         const depositAmount = prompt("Enter a deposit amount: ");
@@ -37,17 +54,19 @@ const getNumberOfLines = () => {
     }
 }
 
-const getBet = (balance) => {
+const getBet = (balance, lines) => {
     while(true) {
-        const bet = prompt("Enter the total bet: ");
+        const bet = prompt("Enter the bet per line: ");
         const numberBet = parseFloat(bet);
 
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
             console.log("Invalid bet, try again");
         } else {
             return numberBet;
         }
     }
 }
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
